754c7c58b124f78ea663fca42c44376a2d55a028
Christophe Hurpeau <christophe@hurpeau.com>
2014-07-30 23:15:35 +0200

    session cookie

--- server.go
+++ server.go
@@ -1,13 +1,55 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"strings"
+	"html/template"
     "net/http"
+    "github.com/gorilla/sessions"
 )
 
+var store = sessions.NewCookieStore([]byte("nightdev"))
+
+type TemplateData struct {
+    Usernames []string
+}
+
+
+const SESSION_NAME = "devnight"
+var indexView, _ = template.ParseFiles("index.html")
+
 func main() {
+	http.Handle("/css", http.FileServer(http.Dir("./static/css")))
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+        session, _ := store.Get(r, SESSION_NAME)
+
+		usernames := session.Values["users"]
+		if usernames == nil {
+			usernames = []string{}
+		} else {
+			usernames = strings.Split(usernames.(string), ",")
+		}
+
+        data := &TemplateData{Usernames: usernames.([]string) }
+
+        indexView.Execute(w, data)
     })
+	http.HandleFunc("/save", func (w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		newUsername := r.Form.Get("name")
+
+		session, _ := store.Get(r, SESSION_NAME)
+		usernames := session.Values["users"]
+		if usernames == nil {
+			usernames = newUsername
+		} else {
+			usernames = usernames.(string) + "," + newUsername
+		}
+		session.Values["users"] = usernames
+
+		session.Save(r, w)
+		http.Redirect(w, r, "/", 302)
+	})
     http.ListenAndServe(":8080", nil)
+	fmt.Print("Listening on 8080")
 }
